d1c8999a9ef60c816531a7152318110d25757230
Muhammad Ihsan Anwar <muhammadihsananwar3@gmail.com>
2025-12-09 10:50:35 +0700

    Update

--- user/proses_register.php
+++ user/proses_register.php
@@ -10,14 +10,14 @@ $email           = $_POST['email'];
 $password        = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
 // Cek apakah email atau username sudah dipakai
-$cek = mysqli_query($conn, "SELECT * FROM users WHERE email='$email' OR username='$username'");
+$cek = mysqli_query($conn, "SELECT * FROM akun_user WHERE email='$email' OR username='$username'");
 if (mysqli_num_rows($cek) > 0) {
     echo "<script>alert('Email atau Username sudah terdaftar!');history.back();</script>";
     exit;
 }
 
 // Input data
-$query = mysqli_query($conn, "INSERT INTO users 
+$query = mysqli_query($conn, "INSERT INTO akun_user 
 (username, nama_lengkap, jenis_kelamin, tanggal_lahir, alamat, email, password)
 VALUES
 ('$username','$nama_lengkap','$jenis_kelamin','$tanggal_lahir','$alamat','$email','$password')");
