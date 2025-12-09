40909066103cf4a68855dc53338981811970f158
Muhammad Ihsan Anwar <muhammadihsananwar3@gmail.com>
2025-12-09 10:30:59 +0700

    Update

--- admin/koneksi.php
+++ admin/koneksi.php
@@ -1,19 +1,11 @@
 <?php
-// Data koneksi
 $host = "localhost";
 $username = "neoz6813_administrator-tb-k4";
 $password = "administrator-online-store";
 $database = "neoz6813_TB-K4-Online-Store";
 
-// Membuat koneksi
-$koneksi = new mysqli($host, $username, $password, $database);
+$koneksi = mysqli_connect($host, $username, $password, $database);
 
-// Cek koneksi
-if ($koneksi->connect_error) {
-    die("Koneksi gagal: " . $koneksi->connect_error);
-} else {
-    echo "Koneksi berhasil ke database!";
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
-
-// Tutup koneksi
-$koneksi->close();
