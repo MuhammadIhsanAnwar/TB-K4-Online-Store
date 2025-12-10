178fa8837dfaa06991d7119114280de916b13227
Muhammad Ihsan Anwar <muhammadihsananwar3@gmail.com>
2025-12-10 13:08:52 +0700

    Update

--- user/verifikasi.php
+++ user/verifikasi.php
@@ -1,14 +1,31 @@
 <?php
-include "..admin/koneksi.php";
+include "../admin/koneksi.php";
 
-$email = $_GET['email'];
-$token = $_GET['token'];
+// Menghindari error karakter URL
+$email = urldecode($_GET['email']);
+$token = urldecode($_GET['token']);
 
-$cek = mysqli_query($koneksi, "SELECT * FROM akun_user WHERE email='$email' AND token='$token'");
+// Prevent SQL injection
+$email = mysqli_real_escape_string($koneksi, $email);
+$token = mysqli_real_escape_string($koneksi, $token);
 
-if (mysqli_num_rows($cek) == 1) {
-    mysqli_query($koneksi, "UPDATE akun_user SET status='1', token='' WHERE email='$email'");
-    echo "<script>alert('Akun berhasil diverifikasi!');window.location='user/login_user.php';</script>";
+// Cek data
+$cek = mysqli_query($koneksi, "
+    SELECT * FROM akun_user 
+    WHERE email='$email' AND token='$token'
+");
+
+if (mysqli_num_rows($cek) === 1) {
+    mysqli_query($koneksi, "
+        UPDATE akun_user 
+        SET status='1', token='' 
+        WHERE email='$email'
+    ");
+
+    echo "<script>
+            alert('Akun berhasil diverifikasi!');
+            window.location='../user/login_user.php';
+          </script>";
 } else {
     echo "Link verifikasi tidak valid atau sudah digunakan.";
 }
