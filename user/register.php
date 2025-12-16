512b1455635947f2ee521e6546ce8a6ce5b8fe09
Muhammad Ihsan Anwar <muhammadihsananwar3@gmail.com>
2025-12-17 01:31:55 +0700

    Update

--- user/register.php
+++ user/register.php
@@ -141,7 +141,7 @@
         <div class="card p-4" style="max-width:420px;width:100%;">
 
             <div class="text-center mb-3">
-                <img src="../images/Background dan Logo/logo.png" alt="Urban Hype Logo" class="login-logo" style="max-width: 80px; height: auto;" />
+                <img src="../images/Background dan Logo/logo.png" alt="Urban Hype Logo" class="login-logo" style="max-width: 160px; height: auto;" />
             </div>
 
             <h4 class="text-center mb-3">Register Akun</h4>
