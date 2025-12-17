15b4b5a30107d607c8be30661e3707491a0f5035
Andika Chairul Ilham <andikachairul283@gmail.com>
2025-12-18 01:58:03 +0700

    update

--- index.php
+++ index.php
@@ -770,27 +770,14 @@ if (isset($_SESSION['user_id'])) {
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <img src="images/Background dan Logo/slide1.jpg" class="d-block w-100" alt="UrbanHype Collection 1">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>New Arrival</h5>
-                    <p>Fresh unisex collection for modern lifestyle</p>
-                </div>
             </div>
 
             <div class="carousel-item">
                 <img src="images/Background dan Logo/slide2.jpg" class="d-block w-100" alt="UrbanHype Collection 2">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Limited Edition</h5>
-                    <p>Bold style with premium quality</p>
-                </div>
             </div>
 
             <div class="carousel-item">
                 <img src="images/Background dan Logo/slide3.jpg" class="d-block w-100" alt="UrbanHype Collection 3">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Urban Essentials</h5>
-                    <p>Comfort meets timeless design</p>
-                </div>
-            </div>
         </div>
 
         <!-- Controls -->
