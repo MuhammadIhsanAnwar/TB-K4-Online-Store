beb37e8917dadee5b6a622d20a145c1769e6a9c9
Muhammad Ihsan Anwar <muhammadihsananwar3@gmail.com>
2025-12-18 09:31:12 +0700

    Update

--- navbar.php
+++ navbar.php
@@ -1,3 +1,18 @@
+<?php
+// Ambil data user jika sudah login
+$user = null;
+if (isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+    $result = mysqli_query($koneksi, "SELECT * FROM akun_user WHERE id='$user_id'");
+    $user = mysqli_fetch_assoc($result);
+}
+
+// Deteksi path relatif berdasarkan lokasi file yang memanggil navbar
+$current_path = $_SERVER['REQUEST_URI'];
+$is_subdir = (strpos($current_path, '/user/produk_pembayaran/') !== false);
+$base_path = $is_subdir ? '../../' : './';
+?>
+
 <style>
     /* ===== NAVBAR STYLING (GLASSMORPHISM + IMPROVED) ===== */
     .navbar {
@@ -342,7 +357,7 @@
 <!-- NAVBAR (dari file baru, diperbaiki & diperkaya) -->
 <nav class="navbar navbar-expand-lg fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="index.php">URBANHYPE</a>
+        <a class="navbar-brand" href="<?php echo $base_path; ?>index.php">URBANHYPE</a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu" aria-controls="navMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -350,10 +365,10 @@
 
         <div class="collapse navbar-collapse" id="navMenu">
             <ul class="navbar-nav ms-auto me-lg-4">
-                <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="user/produk_pembayaran/shop.php">Shop</a></li>
-                <li class="nav-item"><a class="nav-link" href="#about">About Us</a></li>
-                <li class="nav-item"><a class="nav-link" href="#contact">Contact Us</a></li>
+                <li class="nav-item"><a class="nav-link" href="<?php echo $base_path; ?>index.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="<?php echo $base_path; ?>user/produk_pembayaran/shop.php">Shop</a></li>
+                <li class="nav-item"><a class="nav-link" href="<?php echo $base_path; ?>index.php#about">About Us</a></li>
+                <li class="nav-item"><a class="nav-link" href="<?php echo $base_path; ?>index.php#contact">Contact Us</a></li>
             </ul>
 
             <div class="navbar-actions">
@@ -362,8 +377,8 @@
                 </div>
 
                 <?php if ($user): ?>
-                    <a href="user/produk_pembayaran/cart.php" class="cart-link">
-                        <img src="images/cart-icon.png" alt="Cart" style="width:24px; height:24px; filter: var(--cart-icon-filter);">
+                    <a href="<?php echo $base_path; ?>user/produk_pembayaran/cart.php" class="cart-link">
+                        <img src="<?php echo $base_path; ?>images/cart-icon.png" alt="Cart" style="width:24px; height:24px; filter: var(--cart-icon-filter);">
                         <?php
                         $cart_count = isset($_SESSION['cart']) ? count($_SESSION['cart']) : 0;
                         if ($cart_count > 0):
@@ -374,25 +389,37 @@
 
                     <div class="dropdown user-dropdown">
                         <a class="dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="foto_profil/<?php echo htmlspecialchars($user['foto_profil']); ?>"
+                            <img src="<?php echo $base_path; ?>foto_profil/<?php echo htmlspecialchars($user['foto_profil']); ?>"
                                 alt="<?php echo htmlspecialchars($user['nama_lengkap']); ?>"
                                 class="rounded-circle"
                                 style="width:40px; height:40px; object-fit:cover;">
                             <span class="user-name d-none d-lg-inline"><?php echo htmlspecialchars($user['nama_lengkap']); ?></span>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end">
-                            <li><a class="dropdown-item" href="user/settings/settings.php"><i class="bi bi-gear me-2"></i>Setting Akun</a></li>
-                            <li><a class="dropdown-item" href="user/pesanan_saya.php"><i class="bi bi-bag me-2"></i>Pesanan Saya</a></li>
+                            <li><a class="dropdown-item" href="<?php echo $base_path; ?>user/settings/settings.php"><i class="bi bi-gear me-2"></i>Setting Akun</a></li>
+                            <li><a class="dropdown-item" href="<?php echo $base_path; ?>user/pesanan_saya.php"><i class="bi bi-bag me-2"></i>Pesanan Saya</a></li>
                             <li>
                                 <hr class="dropdown-divider">
                             </li>
-                            <li><a class="dropdown-item" href="user/logout_user.php"><i class="bi bi-box-arrow-right me-2"></i>Logout</a></li>
+                            <li><a class="dropdown-item" href="<?php echo $base_path; ?>user/logout_user.php"><i class="bi bi-box-arrow-right me-2"></i>Logout</a></li>
                         </ul>
                     </div>
                 <?php else: ?>
-                    <a href="/user/login_user.php" class="btn btn-dark">Login</a>
+                    <a href="<?php echo $base_path; ?>user/login_user.php" class="btn btn-dark">Login</a>
                 <?php endif; ?>
             </div>
         </div>
     </div>
 </nav>
+
+<script>
+    // Scroll effect untuk navbar
+    window.addEventListener('scroll', function() {
+        const navbar = document.querySelector('.navbar');
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+</script>
