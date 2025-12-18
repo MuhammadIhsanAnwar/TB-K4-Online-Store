bc1a97e321386c52fed490cb5d27c825a3131e7f
Agnes Natalia Siregar <agnes10perkantoran@gmail.com>
2025-12-18 21:00:40 +0700

    Update

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -9,7 +9,7 @@
 
     <!-- CSS -->
     <link rel="stylesheet" href="../css/bootstrap.css">
-    <link rel="stylesheet" href="../css/dashboard_style.css">
+    <link rel="stylesheet" href="css_admin/dashboard_style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
     <!-- Icon -->
